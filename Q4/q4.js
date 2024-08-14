ce2f2808ed3da06db1dde60581090464c30f18b1
Abdullah <150387908+Abdullahateeq00@users.noreply.github.com>
2024-08-14 14:51:34 +0500

    Add files via upload

---
+++ Q4/q4.js
@@ -0,0 +1,4 @@
+"use strict";
+// 4 .Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+let name1 = "Quaid e Azam ";
+console.log(`One said ${name1}, "I dont believe in taking right decision, I take decision and make them right"`);
---
+++ Q4/q4.ts
@@ -0,0 +1,8 @@
+// 4 .Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+
+
+let name1: string = "Quaid e Azam ";
+
+console.log(`One said ${name1}, "I dont believe in taking right decision, I take decision and make them right"`);
+
+
